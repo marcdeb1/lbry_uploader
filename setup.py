5dc075fa6a3f1a0752fd6466fe2bc4cf44ffb648
marcdeb1 <marcdebrouchev@laposte.net>
2018-09-21 17:02:24 +0200

    Rearranged files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='lbry-uploader',
+      version='0.1',
+      description='A simple uploader tool for LBRY',
+      url='https://github.com/marcdeb1/lbry-uploader',
+      author='marcdeb',
+      author_email='marcdebrouchev@laposte.net',
+      license='MIT',
+      packages=['lbry-uploader'],
+      zip_safe=False)
