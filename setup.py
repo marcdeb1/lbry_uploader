a2c34c082c21fd487f9cdfb7fccdb5b4c8583a4c
marcdeb1 <marcdebrouchev@laposte.net>
2018-10-18 05:40:01 +0200

    Requirements and pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='lbry_uploader',
-      version='0.1',
+      version='0.3',
       description='A simple uploader tool for LBRY',
       url='https://github.com/marcdeb1/lbry-uploader',
       author='marcdeb',
